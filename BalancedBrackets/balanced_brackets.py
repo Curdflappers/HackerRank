d2e064e2266775df364ee8929d8efb64ea5bd6af
Mark Wiemer <mwwiemer@outlook.com>
2017-10-14 22:05:28 -0500

    Add Balanced Brackets python solution

---
+++ BalancedBrackets/balanced_brackets.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import sys
+
+def isBalanced(s):
+  if(len(s)) == 0:
+    return True
+  pairing = {"{": "}", "(":")", "[":"]"}
+  first = s[0]
+  try:
+  	match = pairing[first]
+  except:
+    return False
+  
+  openings = 0;
+  for i in range(1, len(s)):
+    if s[i] == first:
+      openings += 1
+    elif s[i] == match and openings == 0:
+      return isBalanced(s[1:i]) and isBalanced(s[i+1:])
+    elif s[i] == match:
+      openings -= 1
+  
+  return False
+
+def trueToYes(val):
+    print(val)
+    if (val == True):
+        return "YES"
+    return "NO"
+
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        s = input().strip()
+        result = isBalanced(s)
+        if result == True:
+            print("YES")
+        else:
+            print("NO")
